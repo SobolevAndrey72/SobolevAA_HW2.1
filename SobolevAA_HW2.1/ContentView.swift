2b9a17f257281e0e74335a82ede6381dcf7a3ab8
Andrey Sobolev <copyprof@gmail.com>
2021-10-19 18:51:37 +0300

    1 Commit

---
+++ SobolevAA_HW2.1/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SobolevAA_HW2.1
+//
+//  Created by Андрей Соболев on 19.10.2021.
+//
+
+import SwiftUI
+
+// 123
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SobolevAA_HW2.1/SobolevAA_HW2_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SobolevAA_HW2_1App.swift
+//  SobolevAA_HW2.1
+//
+//  Created by Андрей Соболев on 19.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SobolevAA_HW2_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
